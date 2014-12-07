d5241d293b11231593c08be6e565d0bf68479263
DelioGomes <a12011@bcc.unifal-mg.edu.br>
2014-12-07 19:23:23 -0200

    Administrador

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :admin_user,     only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -23,6 +24,12 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Usuario deletado"
+    redirect_to users_url
   end
 
   def edit
@@ -50,4 +57,16 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+  
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end 
+
 end
