b54b3d2f64dd9224185f4ad0796eaa37b63863e2
DelioGomes <a12011@bcc.unifal-mg.edu.br>
2014-12-07 16:18:20 -0200

    Login e logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206220334) do
+ActiveRecord::Schema.define(version: 20141207174915) do
 
   create_table "clientes", force: true do |t|
     t.string   "nomeCompleto"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20141206220334) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
